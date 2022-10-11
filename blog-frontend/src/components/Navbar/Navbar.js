0aaeb925efa36a91f49bf62d79e6102421dfe738
Grant Alexander <grant.fricano@gmail.com>
2022-10-11 15:22:23 -0400

    first draft of mobile view

--- blog-frontend/src/components/Navbar/Navbar.js
+++ blog-frontend/src/components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
 import { UserContext } from '../../contexts/UserContext';
 import { Link } from "react-router-dom";
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './Navbar.css';
 
 function Navbar() {
 
   const {token, setToken} = useContext(UserContext);
   const {user, setUser} = useContext(UserContext);
+
+  let [menuClicked, setMenuClicked] = useState(false);
 
   function Logout() {
     setToken(null);
@@ -28,9 +30,11 @@ function Navbar() {
   
   return (
       <nav className="navbar">
-          <h1 className='navbar-name'><Link to='/'>Something With Ginger</Link></h1>
-
-          <ul className='nav-menu'>
+          <h1 className='navbar-name'><Link to='/'>Something With Ginger<i className='fab fa-react'></i></Link></h1>
+          <div className='menu-icon' onClick={() => setMenuClicked(!menuClicked)}>
+            <i className={menuClicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className={menuClicked ? 'nav-menu active': 'nav-menu'}>
             <li className='nav-links'><Link to='/blogs'>Blogs</Link></li>
             <li className='nav-links'><Link to='/newblog'>New Post</Link></li>
             {token ? ( <li className='nav-links'><Link to='/myprofile'>My Profile</Link></li>) : (<li className='nav-links'><Link to='/login'>Log In</Link></li>)  } 
