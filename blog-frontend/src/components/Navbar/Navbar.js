1ffa5957d274ddb3f984a41d7f577ba6ebed3ee9
Grant Alexander <grant.fricano@gmail.com>
2022-10-14 20:33:43 -0400

    more css changes

--- blog-frontend/src/components/Navbar/Navbar.js
+++ blog-frontend/src/components/Navbar/Navbar.js
@@ -43,7 +43,7 @@ function Navbar( {handleLoginClick, setIsCreateAccount} ) {
           <ul className={menuClicked ? 'nav-menu active': 'nav-menu'}>
             <li className='nav-links' onClick={() => setMenuClicked(!menuClicked)}><Link to='/blogs'>Blogs</Link></li>
             <li className='nav-links' onClick={() => setMenuClicked(!menuClicked)}><Link to='/newblog'>New Post</Link></li>
-            {token ? ( <li className='nav-links' onClick={() => setMenuClicked(!menuClicked)}><Link to='/myprofile'>My Profile</Link></li>) : (<li className='nav-links' onClick={(handleClick)}>Log In</li>)  } 
+            {token ? ( <li className='nav-links' onClick={() => setMenuClicked(!menuClicked)}><Link to='/myprofile'>My Profile</Link></li>) : (<li className='nav-links' onClick={(handleClick)}><Link>Log In</Link></li>)  } 
             {token ? ( <li className='nav-links' onClick={() => setMenuClicked(!menuClicked)}><Link to='/' onClick={Logout}>Logout ({user})</Link></li>) : ''}
           </ul>
       </nav>    
